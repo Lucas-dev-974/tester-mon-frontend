d3d67936d5592c4e4e3161d187bbce1ece688244
lucas <lucas.lvn97439@gmail.com>
2023-06-21 16:46:24 +0400

    clean

--- src/components/ItemTable.tsx
+++ src/components/ItemTable.tsx
@@ -1,22 +1,21 @@
-export default function (props: {
+export interface ItemProps {
   id: number;
   name: string;
   editor: string;
   nb_players: number;
-}) {
+}
+
+export default function (props: ItemProps) {
   return (
     <tr>
       <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-300">
-        {" "}
-        {props.name}{" "}
+        {props.name}
       </td>
       <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-300">
-        {" "}
-        {props.editor}{" "}
+        {props.editor}
       </td>
       <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-300">
-        {" "}
-        {props.nb_players}{" "}
+        {props.nb_players}
       </td>
     </tr>
   );
