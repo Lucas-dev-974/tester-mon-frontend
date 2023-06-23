868d37a394fcc1a873bc30ba19190a9467ac4ad0
lucas <lucas.lvn97439@gmail.com>
2023-06-23 08:55:30 +0400

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import type { Component } from "solid-js";
+import { createSignal, type Component, createEffect } from "solid-js";
+import GameService from "./services/GameService";
 
 import DataTable from "./components/Table";
 
 const App: Component = () => {
+  const [data, setData] = createSignal([]);
+
+  createEffect(async () => {
+    setData(await GameService.getAll());
+  });
+
   return (
     <main class="container mx-auto mt-10">
-      <DataTable />
+      <DataTable items={data()} />
     </main>
   );
 };
--- src/services/request.test.ts
+++ src/services/request.test.ts
@@ -1,60 +1,32 @@
-import GameService from "./gameFetch";
+import GameService from "./GameService";
 
 describe("TEST API", () => {
+  beforeEach(() => {
+    global.fetch = jest.fn().mockResolvedValue({
+      ok: true,
+      json: () => Promise.resolve({ message: "Mocked response" }),
+    });
+  });
+
   it("Test get one", async () => {
-    const response = await GameService.get(1);
-    expect(response).toStrictEqual({
-      id: "1",
-      name: "jeu 1",
-      editor: "editeur 1",
-      nb_players: 1,
+    const id = 1;
+    const response = await GameService.get(id);
+
+    // fetch.mockReturnValue(JestMockPromise.resolve(new Response()));
+
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith("http://127.0.0.1:8000/api/game/" + id, {
+      method: "GET",
     });
   });
 
-  it("Test get all", async () => {
-    const response = await GameService.getAll();
-    expect(response).toStrictEqual([
-      {
-        id: "1",
-        name: "jeu 1",
-        editor: "editeur 1",
-        nb_players: 1,
-      },
-      {
-        id: "2",
-        name: "jeu 2",
-        editor: "editeur 2",
-        nb_players: 2,
-      },
-      {
-        id: "3",
-        name: "jeu 3",
-        editor: "editeur 3",
-        nb_players: 3,
-      },
-    ]);
-  });
+  it("Test get one", async () => {
+    const id = 1;
+    const response = await GameService.get(id);
 
-  it("Post create game", async () => {
-    const data = { name: "jeu 4", editor: "4", nb_players: 4 };
-    const response = await GameService.post(data);
-    console.log(response);
-
-    expect(response).toStrictEqual({
-      ...data,
-      id: 4,
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith("http://127.0.0.1:8000/api/game/" + id, {
+      method: "GET",
     });
-  });
-
-  it("Update game", async () => {
-    const data = { id: 4, name: "jeu 4", editor: "4", nb_players: 4 };
-    const response = await GameService.put(data);
-    expect(response).toStrictEqual(data);
-  });
-
-  it("Delete Game", async () => {
-    const id = 4;
-    const response = await GameService.delete(id);
-    expect(response).toBeUndefined();
   });
 });
