a659d9b7c7896e36a0fbb3b0013baab3d0a738b0
lucas <lucas.lvn97439@gmail.com>
2023-06-23 17:37:42 +0400

    setup organisme table

---
+++ src/components/organism/Table.tsx
@@ -0,0 +1,25 @@
+import TableHeadColumn from "../atoms/table/TableHeadColumn";
+import TableBody from "../molecul/table/TableBody";
+import TableHead from "../molecul/table/TableHead";
+import ItemTable, { TableActionEnum } from "../molecul/table/TableRow";
+import { ItemProps } from "../molecul/table/TableRow";
+
+interface tableProps {
+  fields: string[];
+  items: ItemProps[];
+}
+
+const DataTable = (props: tableProps) => {
+  return (
+    <table class="min-w-full divide-y divide-gray-700">
+      <TableHead fields={props.fields} />
+      <TableBody
+        items={props.items}
+        haveAction={true}
+        actions={[TableActionEnum.delete, TableActionEnum.edit]}
+      />
+    </table>
+  );
+};
+
+export default DataTable;
