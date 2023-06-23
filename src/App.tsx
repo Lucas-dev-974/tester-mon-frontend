ae0f8529f3f476d503cf3db3920e8705fbad94d3
lucas <lucas.lvn97439@gmail.com>
2023-06-23 17:39:56 +0400

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,35 @@
 import { createSignal, type Component, createEffect } from "solid-js";
 import GameService from "./services/GameService";
+import Button, { colorEnum } from "./components/atoms/button/Button";
 
-import DataTable from "./components/Table";
+import DataTable from "./components/organism/Table";
+import Modal from "./components/molecul/modal/Modal";
 
 const App: Component = () => {
   const [data, setData] = createSignal([]);
+
+  const [isOpenAddGame, setIsOpenAddGame] = createSignal(false);
+  const toggleIsOpenAddGame = () => setIsOpenAddGame((bool) => !bool);
 
   createEffect(async () => {
     setData(await GameService.getAll());
   });
 
+  const headLabels = ["Nom", "Editeur", "Nombre de joueur", "Action"];
+
   return (
     <main class="container mx-auto mt-10">
-      <DataTable items={data()} />
+      <Button
+        label="Ajouter un jeux"
+        color={colorEnum.green}
+        onClick={toggleIsOpenAddGame}
+      />
+
+      <Modal isOpen={isOpenAddGame} onClose={toggleIsOpenAddGame}>
+        test
+      </Modal>
+
+      <DataTable items={data()} fields={headLabels} />
     </main>
   );
 };
